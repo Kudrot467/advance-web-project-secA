c1e8e39cb72ebb9cc0f47e077cab538ed96333ac
Kudrot467 <kkudrotekhoda@gmail.com>
2024-05-07 14:28:48 +0600

    readme added

--- trip-nest-client/src/app/page.tsx
+++ trip-nest-client/src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="flex items-center justify-center">
       <img src="https://i.ibb.co/MhD3Pbf/trip-nest-logo-removebg-preview.png" className="h-[100] w-[120px]" alt="" />
       <h1 className="text-5xl font-bold text-[#F7B030]"><span className="text-[#38B6FF]">Trip</span> Nest</h1>
     </div>
